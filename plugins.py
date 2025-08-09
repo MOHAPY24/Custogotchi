7b7aa2fb421d368c9cfdeeaad2f25906f8aca74d
Mohammed Mahmoud <166178531+MOHAPY24@users.noreply.github.com>
2025-08-09 19:26:23 +0300

    Add files via upload

--- plugins.py
+++ plugins.py
@@ -1,6 +1,20 @@
 import inspect
 import os
 import time
+from colorama import init, Fore, Back, Style
+
+init()
+
+def printdy(text):
+	for i in text:
+		if i.isdigit():
+			print(Fore.RED + Style.BRIGHT + i, end='', flush=True)
+		else:
+			print(Style.RESET_ALL, end='', flush=True)
+			print(Fore.WHITE + i, end='', flush=True)
+		time.sleep(0.054)
+	print("")
+	return None
 
 
 def debug(msg):
